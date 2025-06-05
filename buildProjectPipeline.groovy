9a4134da006945f68618d0d8fb7bc00bfdf47082
verisoft_user <reut.ishay@verisoft.co>
2025-06-05 20:56:41 +0300

    Added pipeline.

--- buildProjectPipeline.groovy
+++ buildProjectPipeline.groovy
@@ -3,40 +3,34 @@ pipeline {
 
     parameters {
         string(name: 'REPO_URL', defaultValue: 'https://github.com/reutyshai/AutomationFinalProject', description: 'Git repository URL')
-        string(name: 'BRANCH', defaultValue: 'main', description: 'Branch to clone')
     }
 
     environment {
-        PROJECT_DIR = 'downloaded-project'
+        BRANCH = 'main'
     }
 
     stages {
         stage('Clone Repository') {
             steps {
-                echo "Cloning ${params.REPO_URL} (branch: ${params.BRANCH})"
-
-                dir(env.PROJECT_DIR) {
-                    git branch: "${params.BRANCH}",
+                echo "Cloning ${params.REPO_URL} (branch: ${env.BRANCH})"
+                git branch: "${env.BRANCH}",
                         url: "${params.REPO_URL}"
-                }
+
             }
         }
 
         stage('Build Project') {
             steps {
-                dir(env.PROJECT_DIR) {
-                    echo "Running build..."
-                    sh 'mvn clean install'
-                }
+                echo "Running build..."
+                sh 'mvn clean install'
+
             }
         }
 
         stage('Run Project') {
             steps {
-                dir(env.PROJECT_DIR) {
-                    echo "Running application..."
-                    sh 'mvn test'
-                }
+                echo "Running application..."
+                sh 'mvn test'
             }
         }
     }
