a3fa5b5acb6845fa4bbb49e94028478209474b02
verisoft_user <reut.ishay@verisoft.co>
2025-06-05 22:00:57 +0300

    Added ParameterizedCron pipeline.

--- ParameterizedCronPipeline.groovy
+++ ParameterizedCronPipeline.groovy
@@ -1,19 +1,20 @@
 @Library("shared-library") _
 pipeline {
     agent { label 'verisoft-2' }
-    properties([
-            pipelineTriggers([
-                    parameterizedCron([
-                            [cron: 'TZ=Asia/Jerusalem\n02 22 * * *', parameters: [string(name: 'ENV', value: 'dev')]],
-                            [cron: 'TZ=Asia/Jerusalem\n03 22 * * *', parameters: [string(name: 'ENV', value: 'prod')]]
-                    ])
-            ])
-    ])
+
 
     stages {
         stage('Functions') {
             steps {
                 script {
+                    properties([
+                            pipelineTriggers([
+                                    parameterizedCron([
+                                            [cron: 'TZ=Asia/Jerusalem\n02 22 * * *', parameters: [string(name: 'ENV', value: 'dev')]],
+                                            [cron: 'TZ=Asia/Jerusalem\n03 22 * * *', parameters: [string(name: 'ENV', value: 'prod')]]
+                                    ])
+                            ])
+                    ])
                     echo "Start Time: ${currentBuild.startTimeInMillis}"
                 }
             }
