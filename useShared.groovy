96e8feefb8235ba4ec88efe41b58e7d84924e212
verisoft_user <reut.ishay@verisoft.co>
2025-06-05 21:13:51 +0300

    Added pipeline.

--- useShared.groovy
+++ useShared.groovy
@@ -1,7 +1,17 @@
 @Library("shared-library") _
 pipeline {
     agent any
+    parameters {
+        string(name: 'NAME', defaultValue: 'Reut', description: 'Enter your name')
+        booleanParam(name: 'DEBUG_MODE', defaultValue: false, description: 'Enable debug mode?')
+        choice(name: 'ENVIRONMENT', choices: ['DEV', 'QA', 'PROD'], description: 'Choose environment')
+        text(name: 'NOTES', defaultValue: 'No notes', description: 'Additional notes')
+    }
 
+    environment {
+        STATIC_VALUE = "This is a static env var"
+        COMBINED = "${params.NAME}-${params.ENVIRONMENT}"
+    }
     stages {
         stage('Print Hello') {
             steps {
@@ -9,6 +19,8 @@ pipeline {
                     firstFunction()
                     twoFuncs.hello()
                     twoFuncs.bye()
+                    printVariables.printParams()
+                    printVariables.printEnv()
                 }
             }
         }
