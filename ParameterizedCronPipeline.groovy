00df5f4a870888b960b9a1bb1c76e9aaee478778
verisoft_user <reut.ishay@verisoft.co>
2025-06-05 22:10:23 +0300

    Added ParameterizedCron pipeline.

--- ParameterizedCronPipeline.groovy
+++ ParameterizedCronPipeline.groovy
@@ -5,18 +5,19 @@ pipeline {
     parameters {
         string(name: 'NAME', defaultValue: 'Reut', description: 'Enter your name')
         booleanParam(name: 'DEBUG_MODE', defaultValue: false, description: 'Enable debug mode?')
-        choice(name: 'ENVIRONMENT', choices: ['DEV', 'QA', 'PROD'], description: 'Choose environment')}
+        choice(name: 'ENVIRONMENT', choices: ['DEV', 'QA', 'PROD'], description: 'Choose environment')
+    }
     stages {
         stage('Functions') {
             steps {
                 script {
                     properties([
                             pipelineTriggers([
-                                    parameterizedCron(['''
+                                    parameterizedCron('''
                                         TZ=Asia/Jerusalem
                                         07 22 * * *%NAME=Gila;DEBUG_MODE=true
-                                        08 22 * * *%ENVIRONMENT=PROD;DEBUG_MODE=true''',
-                                    ])
+                                        08 22 * * *%ENVIRONMENT=PROD;DEBUG_MODE=true'''
+                                    )
                             ])
                     ])
                     echo "params: ${params}"
